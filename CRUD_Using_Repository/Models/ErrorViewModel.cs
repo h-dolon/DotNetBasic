14aa115d0031e7e9c2b6afd768b2949b71a44568
h-dolon <hosnearadolon@gmail.com>
2024-09-01 21:23:53 +0600

    db connection done

---
+++ CRUD_Using_Repository/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUD_Using_Repository.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CRUD_Using_Repository/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_Using_Repository.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string Name { get; set; } = default!;
+        public string Gender { get; set; } = default!;
+        public string Email { get; set; }= default!;
+         public int PinCode { get; set; }
+        public bool IsActive { get; set; }
+       
+            
+    }
+}
